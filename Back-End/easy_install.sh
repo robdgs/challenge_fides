8d671b8b5bc50170e627e25627830583cc80dc12
Bombatomica64 <qualcosa2001@gmail.com>
2024-09-23 03:25:35 -1100

    Refactor installation scripts and update dependencies list

---
+++ Back-End/easy_install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade && sudo apt autoremove
+
+## Install Python3
+
+sudo apt install python3 -y
+
+## install with apt
+
+sed -n '/##apt/,/##end/p' to_install.txt | sed '1d;$d' | xargs sudo apt install -y
+
+read -p "Enter the path to the virtual environment (or press Enter to create a new one): " venv_path
+
+if [ -z "$venv_path" ]; then
+	venv_path="./venv"
+	python3 -m venv "$venv_path"
+	echo "Created a new virtual environment at $venv_path"
+else
+	if [ ! -d "$venv_path" ]; then
+		echo "The specified path does not exist. Creating a new virtual environment at $venv_path"
+		python3 -m venv "$venv_path"
+	else
+		echo "Using existing virtual environment at $venv_path"
+	fi
+fi
+
+source "$venv_path/bin/activate"
+
+## install with pip
+
+sed -n '/##pip/,/##end/p' to_install.txt | sed '1d;$d' | xargs "$venv_path/bin/pip" install
