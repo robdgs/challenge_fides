8ca1d50839c6bee36e5e569707dcee53d7f9da80
Bombatomica64 <qualcosa2001@gmail.com>
2024-09-23 23:23:01 -1100

    Add Django and PostgreSQL dependencies; update installation instructions

--- Back-End/login/login/settings.py
+++ Back-End/login/login/settings.py
@@ -86,11 +86,22 @@ WSGI_APPLICATION = 'login.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': 'lollo',
+		'USER': 'lore',
+		'PASSWORD' : '123',
+		'HOST': '',
+		'PORT': '5432',
+	}
 }
 
 #user model
@@ -130,7 +141,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'CEST'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
