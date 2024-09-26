7d633ce5ef5790e3ce2c6df8690f110fbbc18283
marcor2742 <marcoruggiero.mail@gmail.com>
2024-09-26 18:39:50 +0200

    test for post for the backend

--- Front-End/src/components/Login/Login.jsx
+++ Front-End/src/components/Login/Login.jsx
@@ -1,59 +1,58 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './index.css';
-import Button from '../Button/Button';
-import Input from '../Input/Input';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./index.css";
+import Button from "../Button/Button";
+import Input from "../Input/Input";
+import { onHandleSubmit } from "../post";
 
 export default function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate();
-	
-  const onHandleSubmit = (e) => {
-    e.preventDefault();
-		if (username && password) {
-			console.log('Username:', username);
-			console.log('Password:', password);
-		} else {
-			console.log('Per favore, inserisci sia username che password.');
-		}
+	const [email, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const navigate = useNavigate();
+
+	const handleSubmit = (e) => {
+    onHandleSubmit(e, email, password, navigate);
   };
 
-  const onRegisterClick = () => {
-    navigate('/register');
-  };
+	const onRegisterClick = () => {
+		navigate("/register");
+	};
 
-  return (
-    <div className="global">
+	return (
+		<div className="global">
 			{/* <div class="background">
         <div class="shape"></div>
         <div class="shape"></div>
 			</div> */}
-      <div className="login_box">
-        <h1>Login</h1>
-        <div className="login_form">
-          <form className="login_form" onSubmit={(e)=> onHandleSubmit(e)}>
-            <Input
-              type="text"
-              name="username"
-              placeholder="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <Input
-              type="password"
-              name="password"
-              placeholder="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <div>
-              <Button text={"Login"} type={"submit"}/>
-              <Button onclick={onRegisterClick} text={"Register"} type={"submit"}/>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+			<div className="login_box">
+				<h1>Login</h1>
+				<div className="login_form">
+					<form className="login_form" onSubmit={(e) => handleSubmit(e)}>
+						<Input
+							type="text"
+							name="username"
+							placeholder="username"
+							value={email}
+							onChange={(e) => setUsername(e.target.value)}
+						/>
+						<Input
+							type="password"
+							name="password"
+							placeholder="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+						<div>
+							<Button text={"Login"} type={"submit"} />
+							<Button
+								onclick={onRegisterClick}
+								text={"Register"}
+								type={"submit"}
+							/>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 }
