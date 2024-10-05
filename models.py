4978531b703472da28a1b4e2a58acbfd9d6a2a83
Bombatomica64 <qualcosa2001@gmail.com>
2024-10-05 17:41:43 +0200

    start

---
+++ models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+class chat_room(models.Model):
+	room_name = models.CharField(max_length=100)
+	room_id = models.IntegerField()
+	room_description = models.TextField()
+	number_of_users = models.IntegerField(default=0)
+
+class chat_message(models.Model):
+	room_id = models.IntegerField()
+	user_id = models.TextField()
+	message = models.TextField()
+	sender = models.TextField()
+	timestamp = models.DateTimeField(auto_now_add=True)
+
+class user(models.Model):
+	user_id = models.IntegerField()
+	username = models.TextField()
+
+def get_user_model(id):
+	return User.objects.get(id=id)
+
+get_user_model().id = models.ForeignKey(User, on_delete=models.CASCADE)
