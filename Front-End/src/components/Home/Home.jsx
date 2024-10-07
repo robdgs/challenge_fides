a4ad5a6f34f95f892310747eb6c6b720c7990834
mruggier <145976814+marcor2742@users.noreply.github.com>
2024-10-07 16:44:42 +0000

    prova home del sito

--- Front-End/src/components/Home/Home.jsx
+++ Front-End/src/components/Home/Home.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import './index.css';
+// import { Nav, navbar } from 'react-bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => {
   return (
     <div className="global">
-      <div className="home_box">
-        <h1>Home</h1>
-        <p>Benvenuto nella pagina Home!</p>
-        {/* Aggiungi il contenuto della tua home page qui */}
-      </div>
+			<div className="navbar">
+				<h1>logo</h1>
+				<p>panino</p>
+				{/* Aggiungi il contenuto della tua home page qui */}
+			</div>
+			<div className="undernavbar">
+				<div className="sidebar"></div>
+				<div className="content"></div>
+			</div>
     </div>
   );
 };
