1edc3ebef66c1ce3c7c6a16146230eddd062dda6
mruggier <145976814+marcor2742@users.noreply.github.com>
2024-10-09 21:51:09 +0000

    chat, manca il componente insieme_di_chat

--- Front-End/src/components/Chat/Chat.jsx
+++ Front-End/src/components/Chat/Chat.jsx
@@ -1,49 +1,54 @@
 import "./Chat.css";
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import Message from "./Message/Message";
+import Input from "../Input/Input";
+import Button from "../Button/Button";
 
-export default function Chat()
-{
-	const [isExpanded, setIsExpanded] = useState(false);
-	const [isExpanded2, setIsExpanded2] = useState(false);
+export default function Chat() {
+  const [isExpanded, setIsExpanded] = useState(false);
 
-	const handleExpand = () => {
-			setIsExpanded(!isExpanded);
-	};
-	const handleExpand2 = () => {
-		setIsExpanded2(!isExpanded2);
-};
+  const handleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
 
-	return (
-		<div className="chat">
-
-			<div className={`chat2 separated ${isExpanded ? 'expanded' : ''}`}>
-				<button className="clickable-div" onClick={handleExpand}>
-					<div className="chat-header">
-						<h1>Chat</h1>
-					</div>
-				</button>
-				{isExpanded && (
-          <div className="expanded-content">
-              <p>Contenuto aggiuntivo</p>
+  return (
+    <div className={`chat separated ${isExpanded ? "expanded" : ""}`}>
+      <div className="chat-button">
+        <button className="clickable-div" onClick={handleExpand}>
+          <div className="chat-header">
+            <h1>Chat</h1>
           </div>
-        )}
-			</div>
-
-			<div className={`chat2 separated ${isExpanded2 ? 'expanded' : ''}`}>
-				<button className="clickable-div" onClick={handleExpand2}>
-					<div className="chat-header">
-						<h1>Chat</h1>
-					</div>
-				</button>
-				{isExpanded2 && (
-          <div className="expanded-content2">
-              <p>Contenuto aggiuntivo</p>
-          </div>
-        )}
-			</div>
-
-		</div>
-
-		
-	);
+        </button>
+      </div>
+      {isExpanded && (
+        <div className="scrollable-content">
+          <Message message={{ text: "Ciao", sender_name: "Anna" }} />
+          <Message message={{ text: "Come state?", sender_name: "Anna" }} />
+          <Message message={{ text: "Così così?", personal_message: true }} />
+          <Message message={{ text: "Bene, tu?", sender_name: "Luca" }} />
+          <Message message={{ text: "Anch'io", sender_name: "Anna" }} />
+          <Message message={{ text: "cosa hai?", sender_name: "Anna" }} />
+          <Message
+            message={{
+              text: "Sto facendo una chat web",
+              personal_message: true,
+            }}
+          />
+          <Message message={{ text: "Che schifo", sender_name: "Luca" }} />
+          <Message
+            message={{
+              text: "volevo dirvi: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              sender_name: "Anna",
+            }}
+          />
+        </div>
+      )}
+      {isExpanded && (
+        <div className="fixed-footer">
+          <Input type="text" placeholder="Scrivi un messaggio..." />
+          <Button text="Invia" />
+        </div>
+      )}
+    </div>
+  );
 }
