b5c9a2001b507e1b5cad5d46102095b732d22a4d
DOPEgduranti <guglielmo.duranti10@gmail.com>
2024-10-11 11:44:34 +0200

    sto facendo qualcosa

--- Back-End/user_manager/user_app/models.py
+++ Back-End/user_manager/user_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class avatars(models.Model):
+    id = models.AutoField(primary_key=True)
+
+class users(models.Model):
+    id = models.AutoField(primary_key=True)
+    account_id = models.IntegerField()
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    birth_date = models.DateField()
+    bio = models.TextField()
+    level = models.DecimalField(..., max_digits=6, decimal_places=3)
+    avatar_id = models.ManyToOneRel(avatars.id)
+
+class friends(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_1 = models.OneToOneField(users.id)
+    user_2 = models.OneToOneField(users.id)
+    
--- Back-End/user_manager/user_app/views.py
+++ Back-End/user_manager/user_app/views.py
@@ -1,3 +1,68 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from .models import users, avatars, friends
+import json
 # Create your views here.
+
+class GetUserParam(APIView):
+	permission_classes = (permissions.AllowAny,)
+	def get(self, request):
+		request_data = request.json()
+		userid = request_data['account_id']
+		user = users.objects.get(account_id=userid)
+		if user:
+			return Response({
+				'level' : user.level
+			}, status=status.HTTP_200_OK)
+		return Response({
+			'error' : 'user not found'
+		}, status=status.HTTP_400_BAD_REQUEST)
+
+class GetUser(APIView):
+	permission_classes = (permissions.AllowAny,)
+	def get(self, request):
+		request_data = request.json()
+		userid = request_data['account_id']
+		user = users.objects.get(account_id=userid)
+		if not user:
+			user = users.objects.create(
+				account_id=userid,
+				first_name='',
+				last_name='',
+				birth_date='0000-00-00',
+				bio='',
+				level=0,
+				avatar_id=-1,
+			)
+			user.save()
+		return Response({
+			'account_id' : user.account_id,
+			'first_name' : user.first_name,
+			'last_name' : user.last_name,
+			'birth_date' : user.birth_date,
+			'bio' : user.bio,
+			'level' : user.level,
+			'avatar_id' : user.avatar_id,
+		}, status=status.HTTP_200_OK)
+
+class SetUser(APIView):
+	def post(self, request):
+		permission_classes = (permissions.AllowAny,)
+		request_data = request.json()
+		userid = request_data['account_id']
+		user = users.objects.get(account_id=userid)
+		if not user:
+			return Response({
+				'error' : 'user not found'
+			}, status=status.HTTP_400_BAD_REQUEST)
+		user.first_name = request_data['first_name']
+		user.last_name = request_data['lastname']
+		user.birth_date = request_data['birth_date']
+		user.bio = request_data['bio']
+		user.save()
+		return Response({
+			'info' : 'changes applied successfully'
+		}, status=status.HTTP_200_OK)
+	
