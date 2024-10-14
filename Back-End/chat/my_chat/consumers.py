7545fa5503c6d2492dcbb457250cb17afe4ea283
Bombatomica64 <qualcosa2001@gmail.com>
2024-10-14 02:31:15 -0700

    Refactor chat application; update WebSocket routing, enhance user registration, and improve message handling

--- Back-End/chat/my_chat/consumers.py
+++ Back-End/chat/my_chat/consumers.py
@@ -1,6 +1,7 @@
 import json
 from channels.generic.websocket import AsyncConsumer
-
+from my_chat.models import UserProfile , chat_room , chat_message
+from .user_services import register_user
 
 class ChatConsumer(AsyncConsumer):
 	async def connect(self):
@@ -8,8 +9,9 @@ class ChatConsumer(AsyncConsumer):
 		if user.is_authenticated:
 			await self.accept()
 		else:
-			await self.close()
-
+			# add the user to the database
+			await register_user(user.username)
+			await self.accept()
 	
 	async def disconnect(self, close_code):
 		pass
@@ -18,18 +20,56 @@ class ChatConsumer(AsyncConsumer):
 		text_data_json = json.loads(text_data)
 		message = text_data_json['message']
 		room_id = text_data_json['room_id']
-		user_id = text_data_json['user_id']
 		sender = text_data_json['sender']
+		timestamp = text_data_json['timestamp']
+
+		# Save message to database
+		message = chat_message.objects.create(
+			room_id=room_id,
+			message=message,
+			sender=sender,
+			timestamp=timestamp
+		)
+		message.save()
+		# Add message to room
 		
-		#if not parse_message(message):
-		#	await self.send(text_data=json.dumps({
-		#		'error': 'Message is invalid'
-		#	}))
-		#	raise ValueError('Message is invalid')
+
+		# Send message to room
+
+
+		await self.send(text_data=json.dumps({
+			'message': message,
+			'room_id': room_id,
+			'timestamp': timestamp,
+			'sender': sender
+		}))
+	async def chat_message(self, event):
+		message = event['message']
+		room_id = event['room_id']
+		user_id = event['user_id']
+		sender = event['sender']
 
 		await self.send(text_data=json.dumps({
 			'message': message,
 			'room_id': room_id,
 			'user_id': user_id,
 			'sender': sender
+		}))
+
+	async def join_room(self, event):
+		room_id = event['room_id']
+		user_id = event['user_id']
+
+		await self.send(text_data=json.dumps({
+			'room_id': room_id,
+			'user_id': user_id
+		}))
+
+	async def leave_room(self, event):
+		room_id = event['room_id']
+		user_id = event['user_id']
+
+		await self.send(text_data=json.dumps({
+			'room_id': room_id,
+			'user_id': user_id
 		}))
